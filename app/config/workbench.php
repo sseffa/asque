00ae6259e709790d9d33820c858f38da22cbcdf4
sseffa <karagozsefa@gmail.com>
2014-02-25 15:41:25 +0200

    added other folders

--- app/config/workbench.php
+++ app/config/workbench.php
@@ -13,7 +13,7 @@ return array(
 	|
 	*/
 
-	'name' => '',
+	'name' => 'Sefa Karagöz',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -26,6 +26,6 @@ return array(
 	|
 	*/
 
-	'email' => '',
+	'email' => 'karagozsefa@gmail.com',
 
 );
